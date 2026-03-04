238b127285184f812a89193f74a567943591d8c1
mzeeshan <would.panther@gmail.com>
2026-03-04 15:55:52 +0300

    feat: notify-error

---
+++ registry/lib/notify-error.ts
@@ -0,0 +1,20 @@
+import { toast } from 'sonner';
+
+export function notifyError(error: unknown, { id }: { id?: string } = {}) {
+  if (import.meta.env.DEV) {
+    console.error(error);
+  }
+
+  if (
+    error &&
+    typeof error === 'object' &&
+    'message' in error &&
+    typeof error.message === 'string'
+  ) {
+    toast.error(error.message, { id });
+  } else if (error instanceof Error) {
+    toast.error(error.message, { id });
+  } else {
+    toast.error('An unknown error occurred', { id });
+  }
+}
